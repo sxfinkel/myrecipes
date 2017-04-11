1d90eb645b5c91ed306e43c96d353d45c069fe38
Susan Barrows <sxbarrows@gmail.com>
2017-04-11 18:03:58 +0000

    updated footer, css, controler, tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
     
     root "pages#home"
     get 'pages/home', to: 'pages#home'
+    
+   #resources :recipes
+    get '/recipes', to: 'recipes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
