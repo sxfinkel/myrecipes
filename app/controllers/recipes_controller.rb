8f28a5e139f782c05899dee026a80bbf84024d2f
Susan Barrows <sxbarrows@gmail.com>
2017-04-11 21:38:53 +0000

    refactoring and adding -page_title

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,9 @@ class RecipesController < ApplicationController
    def index
        @recipes = Recipe.all
    end
+   
+   def show
+      @recipe = Recipe.find(params[:id])
+      
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     
    #resources :recipes
     get '/recipes', to: 'recipes#index'
+    get 'recipes/:id', to: 'recipes#show', as: 'recipe'
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
